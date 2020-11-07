17b31d3115907ecb2f1aea7431b95316fca1be58
dhirajk1108 <66967734+dhirajk1108@users.noreply.github.com>
2020-11-07 22:56:35 +0530

    1st commint
    1st commint

---
+++ athena/athena_weather_hudi_cow.sql
@@ -0,0 +1,28 @@
+CREATE EXTERNAL TABLE `weather_partition_cow`(
+  `_hoodie_commit_time` string,
+  `_hoodie_commit_seqno` string,
+  `_hoodie_record_key` string,
+  `_hoodie_partition_path` string,
+  `_hoodie_file_name` string,
+  `city_id` string,
+  `timestamp` string,
+  `relative_humidity` decimal(3,1),
+  `temperature` decimal(3,1),
+  `absolute_humidity` decimal(5,4)
+  )
+  PARTITIONED BY ( 
+  `date` string)
+ROW FORMAT SERDE
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT
+  'org.apache.hudi.hadoop.HoodieParquetInputFormat'
+OUTPUTFORMAT
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://athena-hudi-bucket/hudi_weather/weather_hudi_cow'
+  
+ALTER TABLE weather_partition_cow ADD
+PARTITION (date = '2020-10-01') LOCATION 's3://athena-hudi-bucket/hudi_weather/weather_hudi_cow/2020-10-01/'
+PARTITION (date = '2020-10-02') LOCATION 's3://athena-hudi-bucket/hudi_weather/weather_hudi_cow/2020-10-02/'
+PARTITION (date = '2020-10-03') LOCATION 's3://athena-hudi-bucket/hudi_weather/weather_hudi_cow/2020-10-03/'
+PARTITION (date = '2020-10-04') LOCATION 's3://athena-hudi-bucket/hudi_weather/weather_hudi_cow/2020-10-04/';
